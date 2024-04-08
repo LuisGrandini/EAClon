cb4084ac305dce2241458046893b6079571e7bd1
pedrucchio <pedrograndini@gmail.com>
2024-04-08 15:00:11 +0200

    Se agrego funcionalidad a los botones

---
+++ updates.js
@@ -0,0 +1,186 @@
+function news() {
+    const sports = document.getElementById("act_link_sports");
+    const news = document.getElementById("act_link_news");
+    const star = document.getElementById("act_link_star");
+    const apex = document.getElementById("act_link_apex");
+    const sims = document.getElementById("act_link_sims");
+    const formula = document.getElementById("act_link_f1");
+    const battle = document.getElementById("act_link_battle");
+    news.style.backgroundColor = "#DEDEDE"
+    news.style.borderBottom = "2px solid #FF4747"
+    sports.style.backgroundColor = "initial"
+    sports.style.borderBottom = "initial"
+    star.style.backgroundColor = "initial"
+    star.style.borderBottom = "initial"
+    apex.style.backgroundColor = "initial"
+    apex.style.borderBottom = "initial"
+    sims.style.backgroundColor = "initial"
+    sims.style.borderBottom = "initial"
+    formula.style.backgroundColor = "initial"
+    formula.style.borderBottom = "initial"
+    battle.style.backgroundColor = "initial"
+    battle.style.borderBottom = "initial"
+}
+
+
+function sport() {
+    const sports = document.getElementById("act_link_sports");
+    const news = document.getElementById("act_link_news");
+    const star = document.getElementById("act_link_star");
+    const apex = document.getElementById("act_link_apex");
+    const sims = document.getElementById("act_link_sims");
+    const formula = document.getElementById("act_link_f1");
+    const battle = document.getElementById("act_link_battle");
+    sports.style.backgroundColor = "#DEDEDE"
+    sports.style.borderBottom = "2px solid #FF4747"
+    news.style.backgroundColor = "initial"
+    news.style.borderBottom = "initial"
+    star.style.backgroundColor = "initial"
+    star.style.borderBottom = "initial"
+    apex.style.backgroundColor = "initial"
+    apex.style.borderBottom = "initial"
+    sims.style.backgroundColor = "initial"
+    sims.style.borderBottom = "initial"
+    formula.style.backgroundColor = "initial"
+    formula.style.borderBottom = "initial"
+    battle.style.backgroundColor = "initial"
+    battle.style.borderBottom = "initial"
+}
+
+function star() {
+    const sports = document.getElementById("act_link_sports");
+    const news = document.getElementById("act_link_news");
+    const star = document.getElementById("act_link_star");
+    const apex = document.getElementById("act_link_apex");
+    const sims = document.getElementById("act_link_sims");
+    const formula = document.getElementById("act_link_f1");
+    const battle = document.getElementById("act_link_battle");
+    star.style.backgroundColor = "#DEDEDE"
+    star.style.borderBottom = "2px solid #FF4747"
+    news.style.backgroundColor = "initial"
+    news.style.borderBottom = "initial"
+    sports.style.backgroundColor = "initial"
+    sports.style.borderBottom = "initial"
+    apex.style.backgroundColor = "initial"
+    apex.style.borderBottom = "initial"
+    sims.style.backgroundColor = "initial"
+    sims.style.borderBottom = "initial"
+    formula.style.backgroundColor = "initial"
+    formula.style.borderBottom = "initial"
+    battle.style.backgroundColor = "initial"
+    battle.style.borderBottom = "initial"
+}
+
+function apex() {
+    const sports = document.getElementById("act_link_sports");
+    const news = document.getElementById("act_link_news");
+    const star = document.getElementById("act_link_star");
+    const apex = document.getElementById("act_link_apex");
+    const sims = document.getElementById("act_link_sims");
+    const formula = document.getElementById("act_link_f1");
+    const battle = document.getElementById("act_link_battle");
+    apex.style.backgroundColor = "#DEDEDE"
+    apex.style.borderBottom = "2px solid #FF4747"
+    news.style.backgroundColor = "initial"
+    news.style.borderBottom = "initial"
+    star.style.backgroundColor = "initial"
+    star.style.borderBottom = "initial"
+    sports.style.backgroundColor = "initial"
+    sports.style.borderBottom = "initial"
+    sims.style.backgroundColor = "initial"
+    sims.style.borderBottom = "initial"
+    formula.style.backgroundColor = "initial"
+    formula.style.borderBottom = "initial"
+    battle.style.backgroundColor = "initial"
+    battle.style.borderBottom = "initial"
+}
+
+function sims() {
+    const sports = document.getElementById("act_link_sports");
+    const news = document.getElementById("act_link_news");
+    const star = document.getElementById("act_link_star");
+    const apex = document.getElementById("act_link_apex");
+    const sims = document.getElementById("act_link_sims");
+    const formula = document.getElementById("act_link_f1");
+    const battle = document.getElementById("act_link_battle");
+    sims.style.backgroundColor = "#DEDEDE"
+    sims.style.borderBottom = "2px solid #FF4747"
+    news.style.backgroundColor = "initial"
+    news.style.borderBottom = "initial"
+    star.style.backgroundColor = "initial"
+    star.style.borderBottom = "initial"
+    apex.style.backgroundColor = "initial"
+    apex.style.borderBottom = "initial"
+    sports.style.backgroundColor = "initial"
+    sports.style.borderBottom = "initial"
+    formula.style.backgroundColor = "initial"
+    formula.style.borderBottom = "initial"
+    battle.style.backgroundColor = "initial"
+    battle.style.borderBottom = "initial"
+}
+
+function formula() {
+    const sports = document.getElementById("act_link_sports");
+    const news = document.getElementById("act_link_news");
+    const star = document.getElementById("act_link_star");
+    const apex = document.getElementById("act_link_apex");
+    const sims = document.getElementById("act_link_sims");
+    const formula = document.getElementById("act_link_f1");
+    const battle = document.getElementById("act_link_battle");
+    formula.style.backgroundColor = "#DEDEDE"
+    formula.style.borderBottom = "2px solid #FF4747"
+    news.style.backgroundColor = "initial"
+    news.style.borderBottom = "initial"
+    star.style.backgroundColor = "initial"
+    star.style.borderBottom = "initial"
+    apex.style.backgroundColor = "initial"
+    apex.style.borderBottom = "initial"
+    sims.style.backgroundColor = "initial"
+    sims.style.borderBottom = "initial"
+    sports.style.backgroundColor = "initial"
+    sports.style.borderBottom = "initial"
+    battle.style.backgroundColor = "initial"
+    battle.style.borderBottom = "initial"
+}
+
+function battle() {
+    const sports = document.getElementById("act_link_sports");
+    const news = document.getElementById("act_link_news");
+    const star = document.getElementById("act_link_star");
+    const apex = document.getElementById("act_link_apex");
+    const sims = document.getElementById("act_link_sims");
+    const formula = document.getElementById("act_link_f1");
+    const battle = document.getElementById("act_link_battle");
+    battle.style.backgroundColor = "#DEDEDE"
+    battle.style.borderBottom = "2px solid #FF4747"
+    news.style.backgroundColor = "initial"
+    news.style.borderBottom = "initial"
+    star.style.backgroundColor = "initial"
+    star.style.borderBottom = "initial"
+    apex.style.backgroundColor = "initial"
+    apex.style.borderBottom = "initial"
+    sims.style.backgroundColor = "initial"
+    sims.style.borderBottom = "initial"
+    formula.style.backgroundColor = "initial"
+    formula.style.borderBottom = "initial"
+    sports.style.backgroundColor = "initial"
+    sports.style.borderBottom = "initial"
+}
+
+
+
+const deporte = document.getElementById("act_link_sports");
+const nuevo = document.getElementById("act_link_news");
+const starw = document.getElementById("act_link_star");
+const apexl = document.getElementById("act_link_apex");
+const sim = document.getElementById("act_link_sims");
+const formul = document.getElementById("act_link_f1");
+const battlef = document.getElementById("act_link_battle");
+deporte.addEventListener("click",sport);
+nuevo.addEventListener("click",news);
+starw.addEventListener("click",star);
+apexl.addEventListener("click",apex);
+sim.addEventListener("click",sims);
+formul.addEventListener("click",formula);
+battlef.addEventListener("click",battle)
+
