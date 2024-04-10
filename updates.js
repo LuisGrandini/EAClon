7c652d0f5156086d2630aaff877099d386de381e
pedrucchio <pedrograndini@gmail.com>
2024-04-10 10:27:04 +0200

    Cambiar imagenes y textos de las demas secciones

--- updates.js
+++ updates.js
@@ -1,184 +1,202 @@
 function news() {
-    const sports = document.getElementById("act_link_sports");
-    const news = document.getElementById("act_link_news");
-    const star = document.getElementById("act_link_star");
-    const apex = document.getElementById("act_link_apex");
-    const sims = document.getElementById("act_link_sims");
-    const formula = document.getElementById("act_link_f1");
-    const battle = document.getElementById("act_link_battle");
-    news.style.backgroundColor = "#DEDEDE"
-    news.style.borderBottom = "2px solid #FF4747"
-    sports.style.backgroundColor = "initial"
-    sports.style.borderBottom = "initial"
+    nuevo.style.backgroundColor = "#DEDEDE"
+    nuevo.style.borderBottom = "2px solid #FF4747"
+    deporte.style.backgroundColor = "initial"
+    deporte.style.borderBottom = "initial"
     star.style.backgroundColor = "initial"
     star.style.borderBottom = "initial"
-    apex.style.backgroundColor = "initial"
-    apex.style.borderBottom = "initial"
-    sims.style.backgroundColor = "initial"
-    sims.style.borderBottom = "initial"
-    formula.style.backgroundColor = "initial"
-    formula.style.borderBottom = "initial"
-    battle.style.backgroundColor = "initial"
-    battle.style.borderBottom = "initial"
+    apexl.style.backgroundColor = "initial"
+    apexl.style.borderBottom = "initial"
+    sim.style.backgroundColor = "initial"
+    sim.style.borderBottom = "initial"
+    formul.style.backgroundColor = "initial"
+    formul.style.borderBottom = "initial"
+    battlef.style.backgroundColor = "initial"
+    battlef.style.borderBottom = "initial"
 }
 
 
 function sport() {
-    const sports = document.getElementById("act_link_sports");
-    const news = document.getElementById("act_link_news");
-    const star = document.getElementById("act_link_star");
-    const apex = document.getElementById("act_link_apex");
-    const sims = document.getElementById("act_link_sims");
-    const formula = document.getElementById("act_link_f1");
-    const battle = document.getElementById("act_link_battle");
-    sports.style.backgroundColor = "#DEDEDE"
-    sports.style.borderBottom = "2px solid #FF4747"
-    news.style.backgroundColor = "initial"
-    news.style.borderBottom = "initial"
+    deporte.style.backgroundColor = "#DEDEDE"
+    deporte.style.borderBottom = "2px solid #FF4747"
+    nuevo.style.backgroundColor = "initial"
+    nuevo.style.borderBottom = "initial"
     star.style.backgroundColor = "initial"
     star.style.borderBottom = "initial"
-    apex.style.backgroundColor = "initial"
-    apex.style.borderBottom = "initial"
-    sims.style.backgroundColor = "initial"
-    sims.style.borderBottom = "initial"
-    formula.style.backgroundColor = "initial"
-    formula.style.borderBottom = "initial"
-    battle.style.backgroundColor = "initial"
-    battle.style.borderBottom = "initial"
+    apexl.style.backgroundColor = "initial"
+    apexl.style.borderBottom = "initial"
+    sim.style.backgroundColor = "initial"
+    sim.style.borderBottom = "initial"
+    formul.style.backgroundColor = "initial"
+    formul.style.borderBottom = "initial"
+    battlef.style.backgroundColor = "initial"
+    battlef.style.borderBottom = "initial"
 }
 
-function star() {
-    const sports = document.getElementById("act_link_sports");
-    const news = document.getElementById("act_link_news");
-    const star = document.getElementById("act_link_star");
-    const apex = document.getElementById("act_link_apex");
-    const sims = document.getElementById("act_link_sims");
-    const formula = document.getElementById("act_link_f1");
-    const battle = document.getElementById("act_link_battle");
+function starw() {
     star.style.backgroundColor = "#DEDEDE"
     star.style.borderBottom = "2px solid #FF4747"
-    news.style.backgroundColor = "initial"
-    news.style.borderBottom = "initial"
-    sports.style.backgroundColor = "initial"
-    sports.style.borderBottom = "initial"
-    apex.style.backgroundColor = "initial"
-    apex.style.borderBottom = "initial"
-    sims.style.backgroundColor = "initial"
-    sims.style.borderBottom = "initial"
-    formula.style.backgroundColor = "initial"
-    formula.style.borderBottom = "initial"
-    battle.style.backgroundColor = "initial"
-    battle.style.borderBottom = "initial"
+    nuevo.style.backgroundColor = "initial"
+    nuevo.style.borderBottom = "initial"
+    deporte.style.backgroundColor = "initial"
+    deporte.style.borderBottom = "initial"
+    apexl.style.backgroundColor = "initial"
+    apexl.style.borderBottom = "initial"
+    sim.style.backgroundColor = "initial"
+    sim.style.borderBottom = "initial"
+    formul.style.backgroundColor = "initial"
+    formul.style.borderBottom = "initial"
+    battlef.style.backgroundColor = "initial"
+    battlef.style.borderBottom = "initial"
 }
 
 function apex() {
-    const sports = document.getElementById("act_link_sports");
-    const news = document.getElementById("act_link_news");
-    const star = document.getElementById("act_link_star");
-    const apex = document.getElementById("act_link_apex");
-    const sims = document.getElementById("act_link_sims");
-    const formula = document.getElementById("act_link_f1");
-    const battle = document.getElementById("act_link_battle");
-    apex.style.backgroundColor = "#DEDEDE"
-    apex.style.borderBottom = "2px solid #FF4747"
-    news.style.backgroundColor = "initial"
-    news.style.borderBottom = "initial"
+    apexl.style.backgroundColor = "#DEDEDE"
+    apexl.style.borderBottom = "2px solid #FF4747"
+    nuevo.style.backgroundColor = "initial"
+    nuevo.style.borderBottom = "initial"
     star.style.backgroundColor = "initial"
     star.style.borderBottom = "initial"
-    sports.style.backgroundColor = "initial"
-    sports.style.borderBottom = "initial"
-    sims.style.backgroundColor = "initial"
-    sims.style.borderBottom = "initial"
-    formula.style.backgroundColor = "initial"
-    formula.style.borderBottom = "initial"
-    battle.style.backgroundColor = "initial"
-    battle.style.borderBottom = "initial"
+    deporte.style.backgroundColor = "initial"
+    deporte.style.borderBottom = "initial"
+    sim.style.backgroundColor = "initial"
+    sim.style.borderBottom = "initial"
+    formul.style.backgroundColor = "initial"
+    formul.style.borderBottom = "initial"
+    battlef.style.backgroundColor = "initial"
+    battlef.style.borderBottom = "initial"
 }
 
 function sims() {
-    const sports = document.getElementById("act_link_sports");
-    const news = document.getElementById("act_link_news");
-    const star = document.getElementById("act_link_star");
-    const apex = document.getElementById("act_link_apex");
-    const sims = document.getElementById("act_link_sims");
-    const formula = document.getElementById("act_link_f1");
-    const battle = document.getElementById("act_link_battle");
-    sims.style.backgroundColor = "#DEDEDE"
-    sims.style.borderBottom = "2px solid #FF4747"
-    news.style.backgroundColor = "initial"
-    news.style.borderBottom = "initial"
+    sim.style.backgroundColor = "#DEDEDE"
+    sim.style.borderBottom = "2px solid #FF4747"
+    nuevo.style.backgroundColor = "initial"
+    nuevo.style.borderBottom = "initial"
     star.style.backgroundColor = "initial"
     star.style.borderBottom = "initial"
-    apex.style.backgroundColor = "initial"
-    apex.style.borderBottom = "initial"
-    sports.style.backgroundColor = "initial"
-    sports.style.borderBottom = "initial"
-    formula.style.backgroundColor = "initial"
-    formula.style.borderBottom = "initial"
-    battle.style.backgroundColor = "initial"
-    battle.style.borderBottom = "initial"
+    apexl.style.backgroundColor = "initial"
+    apexl.style.borderBottom = "initial"
+    deporte.style.backgroundColor = "initial"
+    deporte.style.borderBottom = "initial"
+    formul.style.backgroundColor = "initial"
+    formul.style.borderBottom = "initial"
+    battlef.style.backgroundColor = "initial"
+    battlef.style.borderBottom = "initial"
 }
 
 function formula() {
-    const sports = document.getElementById("act_link_sports");
-    const news = document.getElementById("act_link_news");
-    const star = document.getElementById("act_link_star");
-    const apex = document.getElementById("act_link_apex");
-    const sims = document.getElementById("act_link_sims");
-    const formula = document.getElementById("act_link_f1");
-    const battle = document.getElementById("act_link_battle");
-    formula.style.backgroundColor = "#DEDEDE"
-    formula.style.borderBottom = "2px solid #FF4747"
-    news.style.backgroundColor = "initial"
-    news.style.borderBottom = "initial"
+    formul.style.backgroundColor = "#DEDEDE"
+    formul.style.borderBottom = "2px solid #FF4747"
+    nuevo.style.backgroundColor = "initial"
+    nuevo.style.borderBottom = "initial"
     star.style.backgroundColor = "initial"
     star.style.borderBottom = "initial"
-    apex.style.backgroundColor = "initial"
-    apex.style.borderBottom = "initial"
-    sims.style.backgroundColor = "initial"
-    sims.style.borderBottom = "initial"
-    sports.style.backgroundColor = "initial"
-    sports.style.borderBottom = "initial"
-    battle.style.backgroundColor = "initial"
-    battle.style.borderBottom = "initial"
+    apexl.style.backgroundColor = "initial"
+    apexl.style.borderBottom = "initial"
+    sim.style.backgroundColor = "initial"
+    sim.style.borderBottom = "initial"
+    deporte.style.backgroundColor = "initial"
+    deporte.style.borderBottom = "initial"
+    battlef.style.backgroundColor = "initial"
+    battlef.style.borderBottom = "initial"
 }
 
 function battle() {
-    const sports = document.getElementById("act_link_sports");
-    const news = document.getElementById("act_link_news");
-    const star = document.getElementById("act_link_star");
-    const apex = document.getElementById("act_link_apex");
-    const sims = document.getElementById("act_link_sims");
-    const formula = document.getElementById("act_link_f1");
-    const battle = document.getElementById("act_link_battle");
-    battle.style.backgroundColor = "#DEDEDE"
-    battle.style.borderBottom = "2px solid #FF4747"
-    news.style.backgroundColor = "initial"
-    news.style.borderBottom = "initial"
+    battlef.style.backgroundColor = "#DEDEDE"
+    battlef.style.borderBottom = "2px solid #FF4747"
+    nuevo.style.backgroundColor = "initial"
+    nuevo.style.borderBottom = "initial"
     star.style.backgroundColor = "initial"
     star.style.borderBottom = "initial"
-    apex.style.backgroundColor = "initial"
-    apex.style.borderBottom = "initial"
-    sims.style.backgroundColor = "initial"
-    sims.style.borderBottom = "initial"
-    formula.style.backgroundColor = "initial"
-    formula.style.borderBottom = "initial"
-    sports.style.backgroundColor = "initial"
-    sports.style.borderBottom = "initial"
+    apexl.style.backgroundColor = "initial"
+    apexl.style.borderBottom = "initial"
+    sim.style.backgroundColor = "initial"
+    sim.style.borderBottom = "initial"
+    formul.style.backgroundColor = "initial"
+    formul.style.borderBottom = "initial"
+    deporte.style.backgroundColor = "initial"
+    deporte.style.borderBottom = "initial"
 }
 
 
 
+// Links del menu de arriba
 const deporte = document.getElementById("act_link_sports");
 const nuevo = document.getElementById("act_link_news");
-const starw = document.getElementById("act_link_star");
+const star = document.getElementById("act_link_star");
 const apexl = document.getElementById("act_link_apex");
 const sim = document.getElementById("act_link_sims");
 const formul = document.getElementById("act_link_f1");
 const battlef = document.getElementById("act_link_battle");
+
+//Primeras 6 cards
+const card01 = document.getElementById("act_card01");
+const card02 = document.getElementById("act_card02");
+const card03 = document.getElementById("act_card03");
+const card04 = document.getElementById("act_card04");
+const card05 = document.getElementById("act_card05");
+const card06 = document.getElementById("act_card06");
+
+
+// Segundas 6 cards
+const card11 = document.getElementById("act_card11");
+const card12 = document.getElementById("act_card12");
+const card13 = document.getElementById("act_card13");
+const card14 = document.getElementById("act_card14");
+const card15 = document.getElementById("act_card15");
+const card16 = document.getElementById("act_card16");
+
+
+// Terceras 6 cards
+const card21 = document.getElementById("act_card21");
+const card22 = document.getElementById("act_card22");
+const card23 = document.getElementById("act_card23");
+const card24 = document.getElementById("act_card24");
+const card25 = document.getElementById("act_card25");
+const card26 = document.getElementById("act_card26");
+
+
+// Cuartas 6 cards
+const card31 = document.getElementById("act_card31");
+const card32 = document.getElementById("act_card32");
+const card33 = document.getElementById("act_card33");
+const card34 = document.getElementById("act_card34");
+const card35 = document.getElementById("act_card35");
+const card36 = document.getElementById("act_card36");
+
+
+// Quintas 6 cards
+const card41 = document.getElementById("act_card41");
+const card42 = document.getElementById("act_card42");
+const card43 = document.getElementById("act_card43");
+const card44 = document.getElementById("act_card44");
+const card45 = document.getElementById("act_card45");
+const card46 = document.getElementById("act_card46");
+
+
+// Sextas 6 cards
+const card51 = document.getElementById("act_card51");
+const card52 = document.getElementById("act_card52");
+const card53 = document.getElementById("act_card53");
+const card54 = document.getElementById("act_card54");
+const card55 = document.getElementById("act_card55");
+const card56 = document.getElementById("act_card56");
+
+
+// Septimas 6 cards
+const card61 = document.getElementById("act_card61");
+const card62 = document.getElementById("act_card62");
+const card63 = document.getElementById("act_card63");
+const card64 = document.getElementById("act_card64");
+const card65 = document.getElementById("act_card65");
+const card66 = document.getElementById("act_card66");
+
+
+
+
 deporte.addEventListener("click",sport);
 nuevo.addEventListener("click",news);
-starw.addEventListener("click",star);
+star.addEventListener("click",starw);
 apexl.addEventListener("click",apex);
 sim.addEventListener("click",sims);
 formul.addEventListener("click",formula);
